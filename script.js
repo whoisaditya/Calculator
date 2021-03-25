6a528e0276c20f752c37455bcf87139d5629ea61
whoisaditya <mitraaditya1357@gmail.com>
2021-03-25 23:40:08 +0530

    Draft 1

---
+++ script.js
@@ -0,0 +1,66 @@
+class Calulator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
+        this.clear()
+    }
+
+    clear() {
+        this.currentOperand = ''
+        this.previousOperand = ''
+        this.operation = undefined
+    }
+
+    delete() {
+
+    }
+
+    appendNumber(number) {
+        if(number === '.' && this.currentOperand.includes('.')) return
+        this.currentOperand = this.currentOperand.toString() + number.toString()
+    }
+
+    chooseOperation(operation) {
+        if(this.currentOperand === '') return
+        this.operation = operation
+        this.previousOperand = this.currentOperand
+        this.currentOperand = '' 
+
+   }
+
+   compute() {
+
+   }
+
+   updateDisplay() {
+       this.currentOperandTextElement.innerText = this.currentOperand
+       this.previousOperandTextElement.innerText = this.previousOperand
+
+   }
+}
+
+
+
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalsButton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-delete]')
+const allClearButton = document.querySelector('[data-all-clear]')
+const previousOperandTextElement = document.querySelector('[data-previous-operand]')
+const currentOperandTextElement = document.querySelector('[data-current-operand]')
+
+const calculator = new Calulator(previousOperandTextElement , currentOperandTextElement)
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText)
+        calculator.updateDisplay()
+    })
+})
+
+operationButtons.forEach(button => {
+    button.addEventListener('click', () =>{
+        calculator.appendNumber(button.innerText)
+        calculator.updateDisplay()
+    })
+})
